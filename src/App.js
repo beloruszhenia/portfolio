0eedc43fbfa987520dede1ecd156a12bdda4c8dc
beloruszhenia <beloruszhenia@gmail.com>
2025-09-08 14:27:08 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
